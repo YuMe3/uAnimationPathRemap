77d0a8603e3a46a0c2fe031a9648664ceb615d40
YuMe3 <yume0003@gmail.com>
2023-07-25 02:28:11 +0900

    Unityプロジェクト追加

---
+++ uAnimationPathRemap/Assets/uAnimationPathRemap/Editor/AnimatorContextMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace uAnimationPathRemap.Editor
+{
+    public static class AnimatorContextMenu
+    {
+        [MenuItem("CONTEXT/Animator/PathRemap", validate = true)]
+        private static bool OpenPathRemapValidate(MenuCommand menuCommand)
+        {
+            var animator = menuCommand.context as Animator;
+            return uAnimationPathRemapMainWindow.IsMissingPath(animator);
+        }
+        [MenuItem("CONTEXT/Animator/PathRemap", validate = false)]
+        private static void OpenPathRemap(MenuCommand menuCommand)
+        {
+            var animator = menuCommand.context as Animator;
+            Debug.Log(animator);
+            uAnimationPathRemapMainWindow.ShowWindow(animator);
+        }
+    }
+}
---
+++ uAnimationPathRemap/Assets/uAnimationPathRemap/Editor/uAnimationPathRemapMainWindow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace uAnimationPathRemap.Editor
+{
+    public class uAnimationPathRemapMainWindow : UnityEditor.EditorWindow
+    {
+        public static void ShowWindow(Animator target)
+        {
+            var window = GetWindow<uAnimationPathRemapMainWindow>();
+            window.titleContent = new UnityEngine.GUIContent("uAnimationPathRemap");
+            window._target = target;
+            window.ShowModalUtility();
+        }
+
+        private Animator _target;
+        private void OnGUI()
+        {
+            var clips = _target.runtimeAnimatorController.animationClips;
+        }
+
+        public static bool IsMissingPath(Animator animator)
+        {
+            if (animator == null)
+                return false;
+            if (animator.runtimeAnimatorController == null)
+                return false;
+            
+            var clips = animator.runtimeAnimatorController.animationClips;
+            foreach (var clip in clips)
+            {
+                var bindings = AnimationUtility.GetCurveBindings(clip);
+                foreach (var binding in bindings)
+                {
+                    if (animator.transform.Find(binding.path) == null)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
